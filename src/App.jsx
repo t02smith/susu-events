3c66f77852505761e95057949a0c49cfb993a40f
t02smith <thomas-smith@sky.com>
2021-11-14 17:01:30 +0000

    Updated desktop UI

--- src/App.jsx
+++ src/App.jsx
@@ -24,39 +24,38 @@ function App() {
     const socHook = SocietyHook();
 
     return (
-        <div className="app">
-        
-            {
-                userHook.loggedIn ?
-                <Router>
-                    <Navbar logout={userHook.logout}/>
+        <div className="app-container">
+            <div className="app">         
+                {
+                    userHook.loggedIn ?
+                    <Router>
+                        <Navbar logout={userHook.logout}/>
 
-                    <Route exact path="/events">
-                        <Events avSocs={userHook.user.societies} adminOf={userHook.user.adminOf} getSocEvents={socHook.getSocEvents}/>
-                    </Route>  
-                    <Route exact path="/tracker">
-                        <Tracker isLoaded={gmHook.isLoaded} address={gmHook.address} lngLat={gmHook.addrLngLat}/>
-                    </Route>
-                    <Route exact path="/events/:id" component={EventPage} />
-                    <Route exact path="/tracker/:location" component={Tracker} />
-                    
-                    <Route exact path="/create-event">
-                        <CreateEvent createEvent={eventHook.createEvent} avSocs={userHook.user.adminOf} getSociety={socHook.getSociety}/>
-                    </Route>
-                    <Route exact path="/">
-                        <Profile name={userHook.user.username} societies={userHook.user.societies} getSociety={socHook.getSociety}/>
-                    </Route>
-                    <Route exact path="/societies/:id" component={Society}>
-                        <Society getSociety={socHook.getSociety} getSocEvents={socHook.getSocEvents} />
-                    </Route>
-                </Router>
-                :
-                <Home login={userHook.login}/>
-            }
-                
-
-
+                        <Route exact path="/events">
+                            <Events avSocs={userHook.user.societies} adminOf={userHook.user.adminOf} getSocEvents={socHook.getSocEvents}/>
+                        </Route>  
+                        <Route exact path="/tracker">
+                            <Tracker isLoaded={gmHook.isLoaded} address={gmHook.address} lngLat={gmHook.addrLngLat}/>
+                        </Route>
+                        <Route exact path="/events/:id" component={EventPage} />
+                        <Route exact path="/tracker/:location" component={Tracker} />
+                        
+                        <Route exact path="/create-event">
+                            <CreateEvent createEvent={eventHook.createEvent} avSocs={userHook.user.adminOf} getSociety={socHook.getSociety}/>
+                        </Route>
+                        <Route exact path="/">
+                            <Profile name={userHook.user.username} societies={userHook.user.societies} getSociety={socHook.getSociety}/>
+                        </Route>
+                        <Route exact path="/societies/:id" component={Society}>
+                            <Society getSociety={socHook.getSociety} getSocEvents={socHook.getSocEvents} />
+                        </Route>
+                    </Router>
+                    :
+                    <Home login={userHook.login}/>
+                }
+            </div>
         </div>
+       
     )
 }
 
